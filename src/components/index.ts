fdbdc662e184cee5e14976e0aa0996824ab2378d
rapon1kt <shinnatensei@gmail.com>
2023-08-31 15:42:59 -0300

    feat: export default new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export { default as GoogleIcon } from "./google-icon/google-icon";
+export { default as ColorSchemeToggle } from "./toggle-color-scheme/toggle-color-scheme";
+export { default as AuthAlert } from "./auth-alert/alert";
