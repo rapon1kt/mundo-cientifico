506a187d8010bdf2bb0f389deab70967786d2f08
rapon1kt <shinnatensei@gmail.com>
2023-09-03 15:36:07 -0300

    feat: export new component from components folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as GoogleIcon } from "./google-icon/google-icon";
 export { default as ColorSchemeToggle } from "./toggle-color-scheme/toggle-color-scheme";
 export { default as AuthAlert } from "./auth-alert/alert";
+export { default as TwoSidedLayout } from "./two-sided-layout/two-sided-layout";
