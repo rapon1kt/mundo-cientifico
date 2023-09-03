c49e42934bd2b9f7287e6b56b7c14f958ba91b12
rapon1kt <shinnatensei@gmail.com>
2023-09-03 15:35:32 -0300

    feat: create twoSidedLayout component to home page

---
+++ src/components/two-sided-layout/two-sided-layout.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import { AspectRatio, Container, Box, typographyClasses } from "@mui/joy";
+
+export default function TwoSidedLayout({
+	children,
+	reversed,
+}: React.PropsWithChildren<{ reversed?: boolean }>) {
+	return (
+		<Container
+			sx={(theme) => ({
+				position: "relative",
+				minHeight: "100vh",
+				display: "flex",
+				flexDirection: reversed ? "column-reverse" : "column",
+				alignItems: "center",
+				py: 10,
+				gap: 4,
+				[theme.breakpoints.up(834)]: {
+					flexDirection: "row",
+					gap: 6,
+				},
+				[theme.breakpoints.up(1199)]: {
+					gap: 12,
+				},
+			})}
+		>
+			<Box
+				sx={(theme) => ({
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					gap: "1rem",
+					maxWidth: "50ch",
+					textAlign: "center",
+					flexShrink: 999,
+					[theme.breakpoints.up(834)]: {
+						minWidth: 420,
+						alignItems: "flex-start",
+						textAlign: "initial",
+					},
+					[`& .${typographyClasses.root}`]: {
+						textWrap: "balance",
+					},
+				})}
+			>
+				{children}
+			</Box>
+			<AspectRatio
+				ratio={600 / 520}
+				variant="outlined"
+				maxHeight={300}
+				sx={(theme) => ({
+					minWidth: 300,
+					alignSelf: "stretch",
+					[theme.breakpoints.up(834)]: {
+						alignSelf: "initial",
+						flexGrow: 1,
+						"--AspectRatio-maxHeight": "520px",
+						"--AspectRatio-minHeight": "400px",
+					},
+					borderRadius: "sm",
+					bgcolor: "background.level2",
+					flexBasis: "50%",
+				})}
+			>
+				<img src="/assets/logo.png" alt="" />
+			</AspectRatio>
+		</Container>
+	);
+}
