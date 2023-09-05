7a01ca08b1387008965b492b9861854acc392873
rapon1kt <shinnatensei@gmail.com>
2023-09-04 21:34:55 -0300

    feat: export default main-post component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as GoogleIcon } from "./google-icon/google-icon";
 export { default as ColorSchemeToggle } from "./toggle-color-scheme/toggle-color-scheme";
 export { default as AuthAlert } from "./auth-alert/alert";
 export { default as TwoSidedLayout } from "./two-sided-layout/two-sided-layout";
+export { default as MainPost } from "./main-post/main-post";
