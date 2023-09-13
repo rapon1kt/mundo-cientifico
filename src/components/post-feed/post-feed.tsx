3852368ea9bd1dde598d10b7244f9d9204cbbe51
rapon1kt <shinnatensei@gmail.com>
2023-09-12 22:11:20 -0300

    refactor: remove pots of post-feed component

--- src/components/post-feed/post-feed.tsx
+++ src/components/post-feed/post-feed.tsx
@@ -8,30 +8,7 @@ import {
 	AspectRatio,
 } from "@mui/joy";
 import { useRouter } from "next/navigation";
-
-const posts = [
-	{
-		key: 1,
-		title: "Solve yellow corners of rubik cube",
-		description:
-			"Solving the yellow corners of a Rubik's Cube is typically the last step in solving the entire cube. The method I'll describe here is a basic one, often referred to as the CFOP method (Cross, F2L, OLL, PLL).",
-		link: `/1`,
-	},
-	{
-		key: 2,
-		title: "What is idealism?",
-		description:
-			"Idealism is a philosophical perspective that centers on the belief that reality is fundamentally shaped by the mind or consciousness. It posits that the external world, including the physical universe and everything within it, exists as a product of mental constructs, ideas, or perceptions",
-		link: `/2`,
-	},
-	{
-		key: 3,
-		title: "Foundation of the metaphysics of customs",
-		description:
-			"The 'Foundation of the Metaphysics of Morals' (in German, 'Grundlegung zur Metaphysik der Sitten') is a philosophical work written by Immanuel Kant, one of the most influential philosophers of the Enlightenment era. It was first published in 1785 and is often considered a foundational text in the field of moral philosophy.",
-		link: `/3`,
-	},
-];
+import { posts } from "@/data";
 
 export default function PostFeed() {
 	const router = useRouter();
